d7d485b192555db02e3bb205bec0dd7b7bd3b08c
VISHAL(desktop) <vsaini17@hotmail.com>
2019-09-17 22:57:10 +0530

    about spacing and home hover effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,17 @@ function smoothScroll(offsetVal = 0) {
   }, 800);
 }
 
-$(window).on('hashchange', function(){
-  // Your Code goes here
-  var hash = location.hash.replace( /^#/, '' );
+// $(window).on('hashchange', function(){
+//   // Your Code goes here
+//   var hash = location.hash.replace( /^#/, '' );
   
   
 
- });
-
+//  });
+if($('#work').length && $('#contact').length){
+  var offset_top_work=$('#work').offset().top;
+  var offset_top_contact=$('#contact').offset().top;
+}
 
 
 
@@ -53,5 +56,68 @@ $(document).ready(function () {
       $(".main-navbar").removeClass("shadow-navbar");
     }
   });
+
+
+
+  $('.project-img-1').hover(function(){
+    $('.work-image').toggleClass('hover-image-p1');
+    $('.work-image').toggleClass('hover-project-image');
+  })
+
+  $('.project-img-2').hover(function(){
+    $('.work-image').toggleClass('hover-image-p2');
+    $('.work-image').toggleClass('hover-project-image');
+  })
+
+  $('.project-img-3').hover(function(){
+    $('.work-image').toggleClass('hover-image-p3');
+    $('.work-image').toggleClass('hover-project-image');
+  })
+
+  $('.project-img-4').hover(function(){
+    $('.work-image').toggleClass('hover-image-p4');
+    $('.work-image').toggleClass('hover-project-image');
+  })
+
+  $('.project-img-5').hover(function(){
+    $('.work-image').toggleClass('hover-image-p5');
+    $('.work-image').toggleClass('hover-project-image');
+  })
+  
+// parallel-x 
+
+// $('.banner-section').imageScroll({
+//   // The image to show.
+//   image: null, 
+//   // The data attribute name for images.
+//   // Uses the value of this attribute to load the image.
+//   imageAttribute: 'image', 
+//   // Class added to image holder(s). 
+//   holderClass: 'imageHolder', 
+//   // The element to which the parallax image(s) will be attached to
+//   container: $('body'), 
+//   // The window object which listens to scroll and resize events
+//   windowObject: $(window),
+//   // The speed of the parallax effect.
+//   speed: 0.2, 
+//   // How many percent of the screen each image should cover.
+//   coverRatio: 0.75, 
+//   // The minimum height of the image in pixels.
+//   holderMinHeight: 200, 
+//   // The maximum height of the image in pixels
+//   holderMaxHeight: null,
+//   // Extra height added to the image. 
+//   extraHeight: 0, 
+//   // The original width of the image.
+//   mediaWidth: 1600, 
+//   // The original height of the image.
+//   mediaHeight: 900, 
+//   // Whether or not you want the parallax effect.
+//   parallax: true, 
+//   // Presents a mobile/tablet friendly version, 
+//   // no parallax effect and smaller images 
+//   // (should be used with a mobile/tablet optimized image)
+//   touch: false
+//   });
 
 });
