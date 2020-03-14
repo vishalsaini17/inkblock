a1cb7b403af35a9f1e93d44d679a0c4b5fa0b267
Vishal <vsaini17@hotmail.com>
2020-03-14 15:03:49 +0530

    Databse: set condition in db file

--- database/conn.php
+++ database/conn.php
@@ -1,15 +1,27 @@
 <?php
-$servername='localhost';
-$username='root';
-$password='';
-$dbname='inkblockstudio';
 
-// Create connection
-$conn=mysqli_connect($servername,$username,$password,$dbname);
+$servername='cp-32';
+$username='inkblvo8_admin';
+$password='Jatin!@#321';
+$dbname='inkblvo8_db';
+
+// Create Server Connection
+$conn = mysqli_connect($servername,$username,$password,$dbname);
 
 // Check connection
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+  $servername='localhost';
+  $username='root';
+  $password='';
+  $dbname='inkblockstudio';
+
+  // Create Local Connection
+  $conn = mysqli_connect($servername,$username,$password,$dbname);
+
+  // Check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
 }
 
 $sql = "SELECT * FROM projects ORDER BY id";
